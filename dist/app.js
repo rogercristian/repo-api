883e0dab854f84d1785b6c308c3f8e902a5c2ee3
Roger Cristian <erogennin@gmail.com>
2023-10-25 12:44:01 -0300

    api pro react

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,36 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }/* eslint-disable import/first */
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/first */
 var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+var _path = require('path');
 
 _dotenv2.default.config();
 
 require('./database');
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-var _path = require('path');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
 var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
+
+const whitlist = [
+  'http://192.168.1.4',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitlist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('not aloweed '));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -22,6 +40,8 @@ class App {
   }
 
   middlewares() {
+    this.app.user(_cors2.default.call(void 0, corsOptions));
+    this.app.user(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/first */
 import dotenv from 'dotenv';
+import { resolve } from 'path';
 
 dotenv.config();
 
 import './database';
 
 import express from 'express';
-import { resolve } from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenrRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whitlist = [
+  'http://192.168.1.4',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitlist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('not aloweed '));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -22,6 +40,8 @@ class App {
   }
 
   middlewares() {
+    this.app.user(cors(corsOptions));
+    this.app.user(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
